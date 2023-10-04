85febc934b13b59c5eae1e0637f9b445f3d89632
Vignesh S <vigneshkavery97@gmail.com>
2023-10-05 00:09:49 +0530

    Created Button Class and made separate files

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
 #include "Arduino.h"
 #include "Led.h"
+#include "Button.h"
 
 #define LED_PIN 2
+#define BUTTON_PIN 4
 #define BLINK_DELAY 250
 
 Led led(LED_PIN);
+Button button(BUTTON_PIN);
 
 void setup()
 {
+  Serial.begin(115200);
   led.init();
+  button.init();
 }
 
 void loop()
 {
-  led.on();
-  delay(BLINK_DELAY);
-  led.off();
-  delay(BLINK_DELAY);
+  if(button.isPressed())
+  {
+    led.on();
+    Serial.println("Pressed");
+  }
+  else{
+    led.off();
+  }
 }
