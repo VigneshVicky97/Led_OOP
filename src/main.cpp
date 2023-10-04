2ede74e35f560c47a97b2214200bcbd66302e3a9
Vignesh S <vigneshkavery97@gmail.com>
2023-10-04 23:05:26 +0530

    Removed Serial commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,13 @@
 
 void setup()
 {
-  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED_PIN, HIGH);
-  Serial.println("ON");
   delay(BLINK_DELAY);
   digitalWrite(LED_PIN, LOW);
-  Serial.println("OFF");
   delay(BLINK_DELAY);
 }
