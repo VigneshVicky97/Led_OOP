ae87877910264d4cceb082e030abf6a8906fb671
Vignesh S <vigneshkavery97@gmail.com>
2023-10-04 23:15:22 +0530

    Created Led Class

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,46 @@
 #define LED_PIN 2
 #define BLINK_DELAY 250
 
+class Led
+{
+  private:
+    byte pin;
+  public:
+    Led() {}  // Default Constructor
+
+    Led(byte pin)
+    {
+      this->pin = pin;
+      init();
+    }
+
+    void init()
+    {
+      pinMode(pin, OUTPUT);
+    }
+
+    void on()
+    {
+      digitalWrite(pin, HIGH);
+    }
+
+    void off()
+    {
+      digitalWrite(pin, LOW);
+    }
+};
+
+Led led(LED_PIN);
+
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
+  led.init();
 }
 
 void loop()
 {
-  digitalWrite(LED_PIN, HIGH);
+  led.on();
   delay(BLINK_DELAY);
-  digitalWrite(LED_PIN, LOW);
+  led.off();
   delay(BLINK_DELAY);
 }
