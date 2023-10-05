fdcb54ea932a8bd23b24bdef53296ef2c5fd66a1
vignesh <vigneshs@melss.com>
2023-10-05 17:02:44 +0530

    Created WiFiAP1.cpp file

--- lib/WiFiAP1/WiFiAP1.h
+++ lib/WiFiAP1/WiFiAP1.h
@@ -13,36 +13,11 @@ class WiFiAP1
     const char* password;
     int timeout_counter = 0;
   public:
-    WiFiAP1(const char* ssid, const char* password) 
-    {
-      this->ssid = ssid;
-      this->password = password;
-    }  
-
-    void init()
-    {
-      WiFi.mode(WIFI_STA); //Optional
-      WiFi.begin(ssid, password);
-      Serial.println("\nConnecting");
-      // connect();
-    }
-
-    void connect()
-    {
-      while(WiFi.status() != WL_CONNECTED)
-      {
-        Serial.print(".");
-        delay(200);
-        timeout_counter++;
-        if(timeout_counter >= CONNECTION_TIMEOUT*5)
-        {
-          ESP.restart();
-        }
-      }
-      Serial.println("\nConnected to the WiFi network");
-      Serial.print("Local ESP32 IP: ");
-      Serial.println(WiFi.localIP());
-    }
+    WiFiAP1() {}
+    
+    WiFiAP1(const char* ssid, const char* password);
+    void init();
+    void connect();
 };
 
 #endif
